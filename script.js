eba6ce2310afa64e14565867894e809c630bcc17
ihsanye <ihsany98@gmail.com>
2024-07-18 22:13:17 +0300

    some mistakes fixed

--- script.js
+++ script.js
@@ -1,42 +1,41 @@
-const form = document.querySelector('#todoAddForm')
-const addInput = document.querySelector('#todoName')
-const todoList = document.querySelector('.list-group')
-const firstCardBody = document.querySelectorAll('.card-body')[0]
-const secondCardBody = document.querySelectorAll('.card-body')[1]
-const clearButton = document.querySelectorAll('#clearButton')
+const form = document.querySelector('#todoAddForm');
+const addInput = document.querySelector('#todoName');
+const todoList = document.querySelector('.list-group');
+const firstCardBody = document.querySelectorAll('.card-body')[0];
+const secondCardBody = document.querySelectorAll('.card-body')[1];
+const clearButton = document.querySelectorAll('#clearButton');
 
-runEvents()
+runEvents();
 
 function runEvents() {
-    form.addEventListener('submit', addTodo)
+    form.addEventListener("submit", addTodo);
 }
 
 function addTodo(e) {
     const inputText = addInput.value.trim();
     if (inputText == null || inputText == "") {
-        alert('Lutfen giriniz')
-    } else (
-        addTodoToUI(inputText)
-    )
+        alert("Lutfen giriniz");
+    } else {
+        addTodoToUI(inputText);
+    }
     e.preventDefault();
 }
 
 function addTodoToUI(newTodo) {
-    /*<li class="list-group-item d-flex justify-content-between">Todo 1
-        <a href="#" class="delete-item">
-            <i class="fa fa-remove"></i>
-        </a>
-    </li>*/
-
-    const li = document.createElement('li')
-    li.className = "list-group-item d-flex justify-content-between"
+    const li = document.createElement("li");
+    li.className = "list-group-item d-flex justify-content-between";
     li.textContent = newTodo;
-    const a = createElement('a');
-    a.href = '#';
-    a.className = 'delete-item';
-    const i = createElement('i');
-    i.className = 'fa fa-remove';
-    a.appendChild(i)
-    li.appendChild(a)
-    todoList.appendChild(li)
+
+    const a = document.createElement("a");
+    a.href = "#";
+    a.className = "delete-item";
+
+    const i = document.createElement("i");
+    i.className = "fa fa-remove";
+
+    a.appendChild(i);
+    li.appendChild(a);
+    todoList.appendChild(li);
+
+    addInput.value = "";
 }
